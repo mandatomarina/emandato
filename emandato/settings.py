7a9b035846fc15ef09312436fcd2fb7cb290239b
Pedro Markun <pedro@markun.com.br>
2019-04-14 10:14:37 -0300

    adicionado modulo suits

--- emandato/settings.py
+++ emandato/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'monitordeleis.apps.MonitordeleisConfig',
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
