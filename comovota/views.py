9fa8cb8a722c609667d35d980fabb744084164b9
Pedro Markun <pedro@markun.com.br>
2019-06-28 14:18:33 -0300

    adicionando order_by na lista de votos

--- comovota/views.py
+++ comovota/views.py
@@ -4,7 +4,7 @@ from .models import Voto
 
 # Create your views here.
 def votos_list(request):
-    votos = Voto.objects.all()
+    votos = Voto.objects.all().order_by('-data')
 
     data = {"results": list(votos.values())}
     return JsonResponse(data)
